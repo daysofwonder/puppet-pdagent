d885fd3ef26434a8c21fdb7a1e757fd452830216
Brice Figureau <brice@daysofwonder.com>
2020-09-11 12:57:08 +0200

    Fix config syntax error

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,13 +2,14 @@
 #
 # This class is used to configure PagerDuty agent
 #
-class pdagent::config {
+class pdagent::config(
   $send_interval_secs              = $::pdagent::send_interval_secs,
   $cleanup_interval_secs           = $::pdagent::cleanup_interval_secs,
   $cleanup_threshold_secs          = $::pdagent::cleanup_threshold_secs,
   $backoff_interval_secs           = $::pdagent::backoff_interval_secs,
   $retry_limit_for_possible_errors = $::pdagent::retry_limit_for_possible_errors,
   $source_address                  = $::pdagent::source_address,
+) {
 
   # This template uses:
   #   - send_interval_secs
